b3bcc162d43c9ef4703b1e08c310ab5edb638c43
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2024-05-22 16:31:15 +0330

    add use iamge component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import { db } from "@/server/db";
 import { getImages } from "@/server/queries";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-	const images = await getImages()
+	const images = await getImages();
 	return (
-		<div className="flex flex-wrap gap-4">
+		<div className="flex flex-wrap justify-center gap-4">
 			{images.map(({ id, url, name }) => (
-				<div key={id} className="w-48">
-					<img src={url} alt="" />
+				<div key={id} className="w-48 h-48 flex flex-col ">
+					<Image height={192} width={192}  src={url} alt="" style={{ objectFit: "contain" }} />
 					<div>{name}</div>
 				</div>
 			))}
@@ -22,7 +23,10 @@ export default async function HomePage() {
 	return (
 		<main>
 			<SignedOut>
-				<div className="h-full w-full text-2xl text-center"> Please sign in above </div>
+				<div className="h-full w-full text-2xl text-center">
+					{" "}
+					Please sign in above{" "}
+				</div>
 			</SignedOut>
 			<SignedIn>
 				<Images />

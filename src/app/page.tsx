1659092eb246e911225a366dd32a11fbf19d4fff
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2024-05-22 11:16:01 +0330

    add: use auth for show imasges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
 import { db } from "@/server/db";
-import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
 	const images = await db.query.images.findMany({
 		orderBy(fields, operators) {
 			return operators.desc(fields.id);
 		},
 	});
+	return (
+		<div className="flex flex-wrap gap-4">
+			{images.map(({ id, url, name }) => (
+				<div key={id} className="w-48">
+					<img src={url} alt="" />
+					<div>{name}</div>
+				</div>
+			))}
+		</div>
+	);
+}
 
+export default async function HomePage() {
 	return (
 		<main>
-			<div className="flex flex-wrap gap-4">
-				{images.map(({ id, url, name }) => (
-					<div key={id} className="w-48">
-						<img src={url} alt="" />
-						<div>{name}</div>
-					</div>
-				))}
-			</div>
+			<SignedOut>
+				<div className="h-full w-full text-2xl text-center"> Please sign in above </div>
+			</SignedOut>
+			<SignedIn>
+				<Images />
+			</SignedIn>
 		</main>
 	);
 }
